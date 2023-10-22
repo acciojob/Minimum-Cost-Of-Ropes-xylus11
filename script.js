7de32b236e392711f7f810c619c2e395d3f9063c
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-22 20:08:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 function calculateMinCost() {
-  //your code here
-	let inputElement = document.getElementById("rope-lengths");
-	let resultElement = document.getElementById("result");
+  const ropeLengthsInput = document.getElementById("rope-lengths").value;
+  const ropeLengths = ropeLengthsInput.split(",").map(Number);
 
-	let arr = inputElement.value.split(",");
-	for(let i=0 ; i<arr.length ; i++){
-		arr[i] = parseInt(arr[i]);
-	}
-	console.log(arr);
-	let sum = 0;
-	while(arr.length > 1){
-		arr.sort((a,b) => b-a);
-		let first = arr.pop();
-		let second = arr.pop();
-		let currSum = first + second;
-		sum += currSum;
-		arr.push(currSum);
-	}
-	resultElement.innerText = sum;
-}  
+  // Sort the rope lengths in ascending order
+  ropeLengths.sort((a, b) => a - b);
+
+  let minCost = 0;
+  while (ropeLengths.length > 1) {
+    // Take the two shortest ropes
+    const rope1 = ropeLengths.shift();
+    const rope2 = ropeLengths.shift();
+
+    // Calculate the cost and add it to the total
+    const cost = rope1 + rope2;
+    minCost += cost;
+
+    // Insert the new rope back into the sorted list
+    ropeLengths.push(cost);
+    // Re-sort the list (only the last element is unsorted)
+    ropeLengths.sort((a, b) => a - b);
+  }
+
+  document.getElementById("result").textContent = "Minimum cost: " + minCost;
+}
