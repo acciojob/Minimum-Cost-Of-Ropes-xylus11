ecc8976bc79427a85d20a25cbe1271ac75294ad0
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-22 20:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	let inputElement = document.getElementById("rope-lengths");
+	let resultElement = document.getElementById("result");
+
+	let arr = inputElement.value.split(",");
+	for(let i=0 ; i<arr.length ; i++){
+		arr[i] = parseInt(arr[i]);
+	}
+	console.log(arr);
+	let sum = 0;
+	while(arr.length > 1){
+		arr.sort((a,b) => b-a);
+		let first = arr.pop();
+		let second = arr.pop();
+		let currSum = first + second;
+		sum += currSum;
+		arr.push(currSum);
+	}
+	resultElement.innerText = sum;
 }  
