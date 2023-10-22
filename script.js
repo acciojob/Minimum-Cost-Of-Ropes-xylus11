3a0351ab098c0b4705bd93eba59bbcfd0be58f06
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-22 20:13:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
 function calculateMinCost() {
-  //your code here
-    const input = document.getElementById("rope-lengths").value;
-    const lengths = input.split(",").map(item => parseInt(item.trim()));
-
-    const result = document.getElementById("result");
-    result.innerHTML = minCost(lengths);
-}
-
-function minCost(lengths) {
-    if (lengths.length < 2) {
-        return 0;
-    }
-
-    // Sorting the array initially
-    lengths.sort((a, b) => a - b);
-
+    //your code here
+    let arr = document.getElementById("rope-lengths").value.split(",");
+    
     let totalCost = 0;
-    let currentCost = 0;
+    arr.sort((a,b) => a-b);
+    while(arr.length > 1){
 
-    // Repeatedly connecting the smallest ropes
-    while (lengths.length > 1) {
-        currentCost = lengths[0] + lengths[1];
-        totalCost += currentCost;
-        // Removing the two smallest ropes and adding their combined length
-        lengths.splice(0, 2, currentCost);
-        // Sorting the array again to maintain the order
-        lengths.sort((a, b) => a - b);
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let cost = (parseInt(first) + parseInt(second));
+        totalCost += cost;
+        arr.push(cost);
+
+        arr.sort((a,b) => a-b);
     }
 
-    return totalCost;
-} 
+    // console.log(totalCost);
+	document.getElementById("result").textContent = totalCost;
+  }  
   
-}  
